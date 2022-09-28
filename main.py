95410f49c1bc5db8046ba4d760065ecc88d7483e
Joseph BAca <jojobaca123@gmail.com>
2022-09-27 20:43:58 -0600

    home page

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from flask import Flask, redirect, url_for, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def main_page():
     return render_template("main_page.html")
 
 #Just in case we dont have "/main_page"
-@app.route("/main_page")
-def main_page():
-    return render_template("main_page.html")
+@app.route("/home")
+def home():
+    return render_template("home_page.html")
 
 
 @app.route("/create_project")

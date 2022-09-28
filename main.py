3d700f3fc6ff4fb3278771e9a36b3c1dbfba6303
mjavila4 <mjavila4@miners.utep.edu>
2022-09-28 01:00:29 -0600

    Created data handler for CONFIG data

--- main.py
+++ main.py
@@ -1,36 +1,51 @@
-
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 from datetime import date
+from config_handler import ConfigHandler
 
 app = Flask(__name__)
 
-
 today = date.today()
 today = today.strftime("%m/%d/%Y")
+
+ch = ConfigHandler()
+
 
 @app.route("/")
 def main_page():
     return render_template("main_page.html")
 
-#Just in case we dont have "/main_page"
+
+# Just in case we dont have "/main_page"
 # This one does nothing - Victor Herrera
 @app.route("/home")
 def home():
     return render_template("home_page.html")
-#Project Page
+
+
+# Project Page
 @app.route("/project")
 def project_page():
     return render_template("project_page.html")
 
-#Create Projec Page I made these comments for mysef so I dont get confused.- Victor Herrera
+
+# Create Projec Page I made these comments for mysef so I dont get confused.- Victor Herrera
 @app.route("/create_project")
 def create_project():
     return render_template("Create_Project.html", date=today)
 
-#Edit Configuration page
+
+# Edit Configuration page
 @app.route("/edit_config")
 def edit_project():
     return render_template("edit_config.html", date=today)
+
+
+@app.route("/data_handler", methods=['POST', 'GET'])
+def data_handler():
+    if request.method == 'POST':
+        ch.receive_config_data(request.form)
+    return render_template("project_page.html")
+
 
 if __name__ == "__main__":
     app.run()

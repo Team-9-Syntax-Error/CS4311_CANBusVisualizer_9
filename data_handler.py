39ba0bf86df04c517f7baaada49278947f7c972b
mjavila4 <mjavila4@miners.utep.edu>
2022-09-28 23:43:58 -0600

    Added abstraction to DataHandler, no longer for just config data, still needs retrieve file data functionality

--- data_handler.py
+++ data_handler.py
@@ -5,38 +5,46 @@ from data_object import DataObject
 
 class DataHandler:
     os_char = '/'
-    paths = {"Project Configuration": "config_data"}
-    current_proj_config = DataObject()
+    paths = {"Project Configuration": "config_data", "Node Data": "node_data"}
+    current_proj_config = None
+    current_node = None
 
     def __init__(self):
         if os.name == "nt":
             self.os_char = '\\'
         self.create_dirs()
 
-    def get_data(self, key):
-        return self.current_proj_config.get_data(key)
+    def get(self, data_type, key):
+        data_object_pointer = None
+        if data_type == "Project Configuration":
+            data_object_pointer = self.current_proj_config
+        if data_type == "Node Data":
+            data_object_pointer = self.current_proj_config
+        return data_object_pointer.get(key)
 
-    def receive_data(self, data_type, data):
-        f_dict = {data_type: {}}
-        for key in data:
-            f_dict[data_type].update({key: data[key]})
-        data_object = DataObject(f_dict, data_type)
-        self.current_proj_config = data_object
-        self.dump_to_file(data_object)
+    def receive_data(self, data_type, dictionary):
+        data_object = DataObject({data_type: dictionary})
+        self.create_file(data_object)
+        self.set_current_data_object(data_type, data_object.get_file_name())
 
-    def dump_to_file(self, data_object):
-        json_object = data_object.get_json_data()
-
-        f = open(self.create_path(data_object.get_data_type(), data_object.get_file_name()), "w")
-        f.write(json_object)
-        f.close()
-
-    def set_current_proj(self, data_type, file_name):
-        self.current_proj_config = self.load_from_file(data_type, file_name)
+    def set_current_data_object(self, data_type, file_name):
+        data_object = self.load_from_file(data_type, file_name)
+        if data_type == "Project Configuration":
+            self.current_proj_config = data_object
+        if data_type == "Node Data":
+            self.current_node = data_object
 
     def load_from_file(self, data_type, file_name):
         path = self.create_path(data_type, file_name)
-        return json.load(path)
+        f = open(path, "r")
+        return DataObject(json.load(f))
+
+    def create_file(self, data_object):
+        json_object = json.dumps(data_object.get_data(), indent=4)
+
+        f = open(self.create_path(data_object.get_type(), data_object.get_file_name()), "w")
+        f.write(json_object)
+        f.close()
 
     def create_path(self, data_type, file_name):
         return "{}{}{}.json".format(self.paths[data_type], self.os_char, file_name)
--- data_object.py
+++ data_object.py
@@ -1,45 +1,31 @@
-import json
-
-
 class DataObject:
 
-    def __init__(self, data=None, data_type="Data Type Missing"):
-        if data is None:
-            data = {data_type: {"No Data": ""}}
-        self.data_dict = data
-        self.data_type = data_type
-        self.file_name = self.find_name()
+    def __init__(self, dictionary, file_name=None):
+        self.data = dictionary
+        self.type = DataObject.find_type(dictionary)
+        if file_name is None:
+            self.file_name = self.find_name(dictionary)
+        else:
+            self.file_name = file_name
 
-    def find_name(self):
-        n_dict = self.data_dict[self.data_type]
+    def get(self, key):
+        return self.data[self.type][key]
+
+    def find_name(self, dictionary):
+        n_dict = dictionary[self.get_type()]
 
         for key in n_dict:
             return n_dict[key]
 
-    def get_data(self, key):
-        return self.data_dict[self.data_type][key]
-
-    def get_json_data(self):
-        return json.dumps(self.data_dict, indent=4)
+    def get_type(self):
+        return self.type
 
     def get_file_name(self):
         return self.file_name
 
-    def get_data_type(self):
-        return self.data_type
+    def get_data(self):
+        return self.data
 
-    """
-    def get_value(self, key):
-        temp_pointer = self.config_data
-        return DataObject.recursive_get(key, temp_pointer)
-    
     @staticmethod
-    def recursive_get(key, d):
-        value = 0
-        if key in d:
-            return d.get(key)
-        for di in d:
-            if type(d.get(di)) is dict:
-                value = DataObject.recursive_get(key, d.get(di))
-        return value
-    """
+    def find_type(dictionary):
+        return list(dictionary.keys())[0]

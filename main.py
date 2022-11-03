d343e034502ef3e1431366bb25718c9bfdbadd1f
Mark-Anthony Avila <mjavila4@miners.utep.edu>
2022-11-03 15:42:53 -0600

    Added dialogue window for selecting project directory

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from threading import Thread
 from wsgiref.util import request_uri
 from flask import Flask, redirect, url_for, render_template, request
 from datetime import date
-from data_handler import DataHandler
+from project_handler import ProjectHandler
 import json
 import os
 from threading import Thread
@@ -14,20 +14,19 @@ app = Flask(__name__)
 
 today = date.today()
 today = today.strftime("%m/%d/%Y")
-dh = DataHandler()
 
 # Path 
 path = os.path.dirname(os.path.realpath(__file__))
 os.chdir(path)
 json_folder_path = path + "/json"
 
-#Project Page / Table information is comming
+# Project Page / Table information is comming
 headings = ("Timestamp", "ID", "S", "DL", "Channel", "Annotate")
 data = []
 
 
 # Classes for threads
-read_class = read_bus()
+# read_class = read_bus()
 
 
 # This is our Main Page / First Page that appears
@@ -43,31 +42,41 @@ def main_page():
 
 # Path of where to save
 app.config["UPLOAD_FILES"] = "/GitHub/CS4311_CANBusVisualizer_9/static/img/uploads/"
+
+
 @app.route("/upload_file", methods=["GET", "POST"])
 def upload_file():
     if request.method == "POST":
         if request.files:
-            files = request.files['Upload'] # Access with the tag name 'Upload' that was setup in html
+            files = request.files['Upload']  # Access with the tag name 'Upload' that was setup in html
             print(files)
         return redirect(request.url)
     return render_template("upload_file.html")
 
+
 # Created for 11/2/2022 Demo, just skeleton for sync functionality
 app.config["UPLOAD_FILES"] = "/GitHub/CS4311_CANBusVisualizer_9/static/img/uploads/"
+
+
 @app.route("/sync_project", methods=["GET", "POST"])
 def sync_project():
     if request.method == "POST":
         if request.files:
-            files = request.files['Sync'] # Access with the tag name 'Upload' that was setup in html
+            files = request.files['Sync']  # Access with the tag name 'Upload' that was setup in html
             print(files)
         return redirect(request.url)
     return render_template("sync_project.html")
 
 
 # Create Project Page I made these comments for mysef so I dont get confused.- Victor Herrera
-@app.route("/create_project")
+@app.route("/create_project", methods=["POST", "GET"])
 def create_project():
-    return render_template("Create_Project.html", date=today)
+    if request.method == "POST":
+        ph = ProjectHandler(request.form)
+        ph.save_project()
+        return redirect(url_for("create_project"))
+    else:
+        return render_template("Create_Project.html", date=today)
 
 
 # Edit Configuration page
@@ -82,22 +91,21 @@ def edit_project():
 
 """
 
+
 # Open the Thread to Read on page Open
 @app.route("/project_page", methods=["GET", "POST"])
 def project_page():
-
-    #Creating thread to open socket for reading..
+    # Creating thread to open socket for reading..
     print("Ruinning Thread to recieve BUS")
-    thread_read  = Thread(target = read_class.receiveDBC)
+    thread_read = Thread(target=read_class.receiveDBC)
     thread_read.start()
-        
+
     return render_template("project_page.html", headings=headings, data=data)
 
 
 # WRITE TO CAN BUS SCRIPT
 @app.route('/send')
 def send():
-    
     print('Sending packet...')
     writting = write_bus()
     writting.sendDBC()
@@ -107,47 +115,46 @@ def send():
     while not packet:
         packet = read_class.packet
     writeToTable(packet)
-    
 
     return render_template('project_page.html', headings=headings, data=data)
 
+
 def writeToTable(packet):
-        if packet:
-            packet = str(packet)
-            tokens = packet.split()
-            dl = " ".join(tokens[8:15])
-            channel = tokens[17]
-            annotate = '-'
-            data.append([tokens[1], tokens[3], tokens[5], dl, channel, annotate])
-            print("This is my data", data)
-            #read_class.packet = None
+    if packet:
+        packet = str(packet)
+        tokens = packet.split()
+        dl = " ".join(tokens[8:15])
+        channel = tokens[17]
+        annotate = '-'
+        data.append([tokens[1], tokens[3], tokens[5], dl, channel, annotate])
+        print("This is my data", data)
+        # read_class.packet = None
 
 
-
-
-
-
-
-# Should access Json File of packets and edit Json file 
+# Should access Json File of packets and edit Json file
 @app.route('/edit')
 def edit():
     return render_template('project_page.html')
 
-# 
+
+#
 @app.route('/annotate')
 def annotate():
     return render_template('project_page.html')
+
 
 # Should replay the saved packet json file
 @app.route('/replay')
 def replay():
     return render_template('project_page.html')
 
+
 # Should save a json file of packets refer to print("My data: ", tokens[1], tokens[3], tokens[5], myvar) line of code
 @app.route('/save')
 def save():
     return render_template('project_page.html')
 
+
 if __name__ == "__main__":
-    #Allows updates on page without running program over again. 
+    # Allows updates on page without running program over again.
     app.run(debug=True)
---
+++ project_handler.py
@@ -0,0 +1,68 @@
+import os
+import json
+import tkinter as tk
+from tkinter import filedialog
+
+
+class ProjectHandler:
+    os_char = '/'
+    json_object = ""
+
+    def __init__(self, project_info):
+        if os.name == "nt":
+            self.os_char = '\\'
+        self.json_object = json.dumps(project_info, indent=2)
+
+    def save_project(self):
+        root = tk.Tk()
+        root.withdraw()
+
+        file_path = filedialog.askdirectory()
+        print(file_path)
+
+    def get(self, data_type, key):
+        data_object_pointer = None
+        if data_type == "Project Configuration":
+            data_object_pointer = self.current_proj_config
+        if data_type == "Node Data":
+            data_object_pointer = self.current_proj_config
+        return data_object_pointer.get(key)
+
+    def receive_data(self, data_type, dictionary):
+        data_object = DataObject({data_type: dictionary})
+        self.create_file(data_object)
+        self.set_current_data_object(data_type, data_object.get_file_name())
+
+    def set_current_data_object(self, data_type, file_name):
+        data_object = self.load_from_file(data_type, file_name)
+        if data_type == "Project Configuration":
+            self.current_proj_config = data_object
+        if data_type == "Node Data":
+            self.current_node = data_object
+
+    def load_from_file(self, data_type, file_name):
+        path = self.create_path(data_type, file_name)
+        f = open(path, "r")
+        return DataObject(json.load(f))
+
+    def create_file(self, data_object):
+        json_object = json.dumps(data_object.get_data(), indent=4)
+
+        f = open(self.create_path(data_object.get_type(), data_object.get_file_name()), "w")
+        f.write(json_object)
+        f.close()
+
+    def create_path(self, data_type, file_name):
+        return "{}{}{}.json".format(self.paths[data_type], self.os_char, file_name)
+
+    def create_dirs(self):
+        for path in self.paths:
+            if not os.path.exists(self.paths[path]):
+                os.mkdir(self.paths[path])
+
+    def path_de_traverse(self, path, levels=1):
+        split_path = path.split(self.os_char)
+        final_path = self.os_char.join(split_path[:-levels])
+        if final_path == '':
+            raise Exception("Path Out of Bounds")
+        return final_path

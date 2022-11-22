d60d7941901e913a965f7cbfa4c5df187196df27
mjavila4 <60335477+mjavila4@users.noreply.github.com>
2022-11-21 21:06:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -83,17 +83,16 @@ def sync_project():
 
     return render_template('sync_project.html', Success="")
 
+@app.route("/get_form", methods=["POST", "GET"])
+def get_form():
+    if FileHandler.save_project(request.form) == 0:
+        return "fwafw"
+    return '', 400
 
 # Create Project Page I made these comments for mysef so I dont get confused.- Victor Herrera
-@app.route("/create_project", methods=["POST", "GET"])
+@app.route('/create_project')
 def create_project():
-    if request.method == "POST":
-        FileHandler.save_project(request.form)
-        # FileHandler.load_project()
-        return redirect(url_for("project_page"))
-    else:
-        return render_template("Create_Project.html", date=today)
-
+    return render_template('Create_Project.html')
 
 # Edit Configuration page
 # When working on the project this will allow the user to modify the configuration

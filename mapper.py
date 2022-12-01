e128a8365450212abfa5de50f883569abb687317
JosephB <josephb@Joseph.kali.jx>
2022-11-30 21:50:57 -0700

    map builder is better

--- mapper.py
+++ mapper.py
@@ -10,27 +10,40 @@ class Mapper():
     # Inserts new data into map.json
     def build_map(self):
         
-        node_name = self.grab_nodes()
+        node_names = self.grab_nodes()
 
         # Can only pass if, if not None
-        if node_name:
-            data =  {"key":self.key , "name": node_name}
-            self.map.append(data)
-            self.insert_node()
-            
+        if node_names:
+            for node_name in node_names: 
+                data =  {"key":self.key , "parent": 0, "name": node_name}
+                self.key+=1
+                self.map.append(data)
+                self.insert_node()
+
     # Grabs name of the node
     def grab_nodes(self):
-        print(self.project_path)
 
         with open(self.project_path + "/decoded_data_json.json") as f:
             node = json.load(f)
-        print(node)
 
-        for node_name in node[0].keys():
-            print(node_name)
-            if node_name not in self.map:
-                return node_name
-        return None
+        with open(self.project_path + "/map.json") as f:
+            json_loaded_nodes = json.load(f)
+
+        loaded_nodes = []
+
+        for packet in json_loaded_nodes :
+            name = list(packet.values())[-1]
+            loaded_nodes.append(name)
+
+        non_graphed_nodes = []
+
+        for i in range(len(node)):
+            node_name = list(node[i].keys())[0]
+            if node_name not in loaded_nodes:
+                non_graphed_nodes.append(node_name)
+                print(node_name)
+
+        return non_graphed_nodes
 
     def insert_node(self):
         with open(self.project_path + "/map.json", "w") as jsonFile:
@@ -40,4 +53,3 @@ class Mapper():
         with open("Current_Working_Project.json", "r") as jsonFile:
                     data = json.load(jsonFile)
                     return data["Project_path"]
-
